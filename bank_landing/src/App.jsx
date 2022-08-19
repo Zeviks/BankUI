5163deb4f8ddb08813892dd9ed3f61bc9a39aa7e
Natanael Pacheco <natepacheco716@gmail.com>
2022-08-18 19:16:29 -0500

    Created all necessary base components.

--- bank_landing/src/App.jsx
+++ bank_landing/src/App.jsx
@@ -1,9 +1,43 @@
 import React from 'react'
+import styles from './style'
 
-const App = () => {
-  return (
-    <div className='text-center'>App</div>
-  )
-}
+import { 
+  Navbar,
+  Billing,
+  CardDeal,
+  Business,
+  Clients,
+  CTA,
+  Stats,
+  Footer,
+  Testimonials,
+  Hero,
+ } from './components'
+
+
+const App = () => (
+    <div className='bg-primary w-full overflow-hidden'>
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          Navbar
+        </div>
+      </div>
+
+      <div className={`bg-primary ${styles.flexStart}`}>
+        <div className={styles.boxWidth}>
+          Hero
+        </div>
+      </div>
+
+      <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+        <div className={styles.boxWidth}>
+          Stats
+          Business
+          Billing
+        </div>
+      </div>
+
+    </div>
+);
 
 export default App
